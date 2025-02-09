5abedd560b8084fd3e1f94569e7c42c31d44d6b7
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-02-09 21:44:09 +0100

    changed bool to string as return value of registering user

--- Repositories/IAuthRepository.cs
+++ Repositories/IAuthRepository.cs
@@ -4,7 +4,7 @@ namespace restAPI.Repositories;
 
 public interface IAuthRepository
 {
-    Task<bool> AddRaportToAcceptByAdmin(RegisterDto registerDto);
+    Task<string> AddRaportToAcceptByAdmin(RegisterDto registerDto);
     Task<bool> ActivateAccount(long id);
     Task<List<User>> GetUsers();
 }
