ce7116f0acf290b539918bde7734d0c1c09f813c
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-02-09 21:44:44 +0100

    added to User model field Email and changed Username from long to string

--- Models/LoginDto.cs
+++ Models/LoginDto.cs
@@ -2,6 +2,6 @@
 
 public class LoginDto
 {
-    public long Username { get; set; }
+    public string Username { get; set; }
     public string Password { get; set; }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -5,4 +5,5 @@ public class User
     public long Id { get; set; }
     public string Password { get; set; }
     public string Role { get; set; }
+    public string Email { get; set; }
 }
